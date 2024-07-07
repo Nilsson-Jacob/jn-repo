30bb1ba2772d45fa98801cfcb77e621769833e2f
nilsson-jacob <jacobnilsson97@gmail.com>
2024-07-07 22:09:01 +0200

    testing2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import background from "./images/tree.jpg"
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import background from "./images/tree.jpg";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
-
-import About from './About';
-import Nav from './Nav';
+import About from "./About";
+import Nav from "./Nav";
 
 function App() {
-  return (  
-    <Router>
-    <div> 
-  <img src={background} className="img-fluid" alt="Responsive image" width="100%"></img>
-  <Nav/>
-    <Routes>
-    <Route path='/about' element={<About/>} />
-    {/* Content */}
-
-    </Routes>
-
-
-
-  </div>  
-  </Router>   
+  return (
+    <HashRouter>
+      <div>
+        <img
+          src={background}
+          className="img-fluid"
+          alt="Responsive image"
+          width="100%"
+        ></img>
+        <Nav />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          {/* Content */}
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
